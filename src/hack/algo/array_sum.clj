663c17f8ed458280753560bcd80c007e8981a764
Robert Mitchell <robert.mitchell36@gmail.com>
2021-01-09 11:41:09 -0600

    Add initial project structure; add algs array sum solution

---
+++ src/hack/algo/array_sum.clj
@@ -0,0 +1,4 @@
+(ns hack.algo.array-sum)
+
+(defn simpleArraySum [ar]
+  (reduce + 0 ar))
---
+++ test/hack/algo/array_sum_test.clj
@@ -0,0 +1,10 @@
+(ns hack.algo.array-sum-test
+  (:require [clojure.test :refer [deftest is]]
+            [hack.algo.array-sum :refer [simpleArraySum]]))
+
+(deftest aray-sum-test
+  (is (= 31 (simpleArraySum [1 2 3 4 10 11])))
+
+  (is (= 10 (simpleArraySum [1 2 3 4])))
+
+  (is (= 0 (simpleArraySum [1 -1 2 -2 3 -3]))))
