e60d5f9445f83dc7020fdb06ff696d572a079423
Robert Mitchell <robert.mitchell36@gmail.com>
2021-02-01 13:36:07 -0600

    Simplify math restaurant solution

--- src/hack/math/restaurant.clj
+++ src/hack/math/restaurant.clj
@@ -1,9 +1,4 @@
 (ns hack.math.restaurant)
 
-(defn gcd-sqr [a b]
-  (if (zero? b)
-    (* a a)
-    (recur b (mod a b))))
-
 (defn restaurant [l b]
-  (/ (* l b) (gcd-sqr l b)))
+  (/ (* l b) (.. (biginteger l) (gcd (biginteger b)) (pow 2))))
