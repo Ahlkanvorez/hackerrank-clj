99b94965895530c55e7a6d3eb2cd40648ba13ce4
Robert Mitchell <robert.mitchell36@gmail.com>
2021-02-04 16:39:25 -0600

    Improve the fp string compression solution

--- src/hack/fp/string_compression.clj
+++ src/hack/fp/string_compression.clj
@@ -1,18 +1,8 @@
 (ns hack.fp.string-compression)
 
 (defn run-length-encode [s]
-  (loop [c (first s)
-         s (rest s)
-         n 1
-         accum (transient [])]
-    (if-let [k (first s)]
-      (if (= c k)
-        (recur c (rest s) (inc n) accum)
-        (if (= n 1)
-          (recur k (rest s) 1 (conj! accum c))
-          (recur k (rest s) 1 (conj! (conj! accum c) n))))
-      (->> (if (= n 1)
-             (conj! accum c)
-             (conj! (conj! accum c) n))
-           persistent!
-           (apply str)))))
+  (->> (partition-by identity s)
+       (transduce (comp (mapcat (juxt first count))
+                        (remove #{1}))
+                  conj)
+       (apply str)))
